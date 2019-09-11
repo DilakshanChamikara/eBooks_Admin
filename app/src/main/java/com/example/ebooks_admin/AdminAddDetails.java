cc4068e87da7fd1ece6cbf3989979c858536590a
Dilakshan Chamikara <wvdilakshanchamikara@gmail.com>
2019-09-11 15:45:20 +0530

    initial commit

--- app/src/main/java/com/example/ebooks_admin/AdminAddDetails.java
+++ app/src/main/java/com/example/ebooks_admin/AdminAddDetails.java
@@ -5,11 +5,58 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView;
 
-public class AdminAddDetails extends AppCompatActivity {
 
-    private Button cnl;
+public class AdminAddDetails extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    private Button cnl, AddBtn, ClearBtn;
+
+    //spinner arrays
+    String[] bookCategory = {"",
+            "Action and Adventure",
+            "Anthology",
+            "Biography/Autobiography",
+            "Classic",
+            "Comic and Graphic Novel",
+            "Crime and Detective",
+            "Drama",
+            "Essay",
+            "Fable",
+            "Fairy Tale",
+            "Fan-Fiction",
+            "Fantasy",
+            "Historical Fiction",
+            "Horror",
+            "Humor",
+            "Legend",
+            "Magical Realism",
+            "Memoir",
+            "Mystery",
+            "Mythology",
+            "Narrative Nonfiction",
+            "Periodicals",
+            "Poetry",
+            "Realistic Fiction",
+            "Reference Books",
+            "Romance",
+            "Satire",
+            "Science Fiction (Sci-Fi)",
+            "Self-help Book",
+            "Short Story",
+            "Speech",
+            "Suspense/Thriller"
+    };
+
+    String[] language = {"",
+            "English",
+            "Sinhala",
+            "Tamil"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +71,55 @@ public class AdminAddDetails extends AppCompatActivity {
                 openAdminMenu();
             }
         });
+
+        //clear All button
+        ClearBtn = (Button) findViewById(R.id.clear);
+        ClearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clearForm();
+            }
+        });
+
+        //category spinner
+        Spinner spinner = (Spinner) findViewById(R.id.category);
+        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter ArrCate = new ArrayAdapter(this,android.R.layout.simple_spinner_item, bookCategory);
+        ArrCate.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(ArrCate);
+
+        //language spinner
+        Spinner spinner1 = (Spinner) findViewById(R.id.lang);
+        spinner1.setOnItemSelectedListener(this);
+        ArrayAdapter ArrLang = new ArrayAdapter(this, android.R.layout.simple_spinner_item, language);
+        ArrLang.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(ArrLang);
     }
+
 
     //cancel button
     public void openAdminMenu() {
         Intent intent = new Intent(this, AdminMenu.class);
         startActivity(intent);
+    }
+
+
+    //clear All button
+    private void clearForm() {
+        startActivity(new Intent(this, AdminAddDetails.class));
+        overridePendingTransition(0,0);
+        this.finish();
+    }
+
+
+    //spinner methods
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View view, int pos, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
