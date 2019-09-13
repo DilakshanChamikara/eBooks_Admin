a673bd9abeb58559d4012a08d17fcaaa48070a13
Dilakshan Chamikara <wvdilakshanchamikara@gmail.com>
2019-09-13 16:47:18 +0530

    initial

--- app/src/main/java/com/example/ebooks_admin/AdminAddDetails.java
+++ app/src/main/java/com/example/ebooks_admin/AdminAddDetails.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.AdapterView;
 
@@ -16,6 +17,8 @@ import android.widget.AdapterView;
 public class AdminAddDetails extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     private Button cnl, AddBtn, ClearBtn;
+    private EditText ISBN, TITLE, AUTHER, SIZE, INTRO, RPrice, FPrice;
+    private Spinner CATEGORY, LANG;
 
     //background Animation
     ConstraintLayout constraintLayout;
@@ -32,6 +35,28 @@ public class AdminAddDetails extends AppCompatActivity implements AdapterView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_details);
+
+        //find Ids
+        ISBN = (EditText) findViewById(R.id.isbn);
+        TITLE = (EditText) findViewById(R.id.title);
+        AUTHER = (EditText) findViewById(R.id.auther);
+        SIZE = (EditText) findViewById(R.id.size);
+        INTRO = (EditText) findViewById(R.id.intro);
+        RPrice = (EditText) findViewById(R.id.rPrice);
+        FPrice = (EditText) findViewById(R.id.fPrice);
+
+        CATEGORY = (Spinner) findViewById(R.id.category);
+        LANG = (Spinner) findViewById(R.id.lang);
+
+
+        //add button
+        AddBtn = (Button) findViewById(R.id.addButton);
+        AddBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
         //cancel button
         cnl = (Button) findViewById(R.id.cancel);
@@ -67,6 +92,20 @@ public class AdminAddDetails extends AppCompatActivity implements AdapterView.On
         animationDrawable.setExitFadeDuration(4500);
         animationDrawable.start();
 
+    }
+
+    //add button
+    private void addRecord(){
+        String isbn = ISBN.getText().toString().trim();
+        String title = TITLE.getText().toString().trim();
+        String auther = AUTHER.getText().toString().trim();
+        String size = SIZE.getText().toString().trim();
+        String intro = INTRO.getText().toString().trim();
+        String rPrice = RPrice.getText().toString().trim();
+        String fPrice = FPrice.getText().toString().trim();
+
+        String category = CATEGORY.getSelectedItem().toString();
+        String lang = LANG.getSelectedItem().toString();
     }
 
 
