fe14eb36789b3f02cfa368ec93e92656ea2f53d0
Dilakshan Chamikara <wvdilakshanchamikara@gmail.com>
2019-09-13 14:48:17 +0530

    initial

--- app/src/main/java/com/example/ebooks_admin/AdminLogin.java
+++ app/src/main/java/com/example/ebooks_admin/AdminLogin.java
@@ -5,18 +5,47 @@ import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.VideoView;
 
 public class AdminLogin extends AppCompatActivity {
 
     private Button btn1, cancel;
+    private VideoView videoBG;
+    MediaPlayer mMediaPlayer;
+    int mCurrentVideoPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
+
+        //video view
+        videoBG = (VideoView) findViewById(R.id.videoView);
+        //Build video uri
+        Uri uri = Uri.parse("android.resource://"
+                            + getPackageName()
+                            + "/"
+                            + R.raw.video);
+
+        videoBG.setVideoURI(uri);
+        videoBG.start();
+
+        videoBG.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mMediaPlayer = mediaPlayer;
+                mMediaPlayer.setLooping(true);
+                if(mCurrentVideoPosition != 0){
+                    mMediaPlayer.seekTo(mCurrentVideoPosition);
+                    mMediaPlayer.start();
+                }
+            }
+        });
 
         //Login button
         btn1 = (Button) findViewById(R.id.buttonLog);
@@ -37,6 +66,27 @@ public class AdminLogin extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mCurrentVideoPosition = mMediaPlayer.getCurrentPosition();
+        videoBG.pause();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        videoBG.resume();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        mMediaPlayer.release();
+        mMediaPlayer = null;
+    }
+
 
     //login method
     public void openMenu(){
