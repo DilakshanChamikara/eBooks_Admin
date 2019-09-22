b07ce432507aeb254198a8ac4de75779dba9beba
Dilakshan Chamikara <wvdilakshanchamikara@gmail.com>
2019-09-22 23:28:49 +0530

    initial

--- app/src/main/java/com/example/ebooks_admin/BookList.java
+++ app/src/main/java/com/example/ebooks_admin/BookList.java
@@ -6,6 +6,9 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.mancj.materialsearchbar.MaterialSearchBar;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -13,11 +16,16 @@ public class BookList extends AppCompatActivity {
 
     RecyclerView recyclerView;
 
+    //search function
+    List<String> suggestList = new ArrayList<>();
+    MaterialSearchBar materialSearchBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
 
+        //recycler View
         recyclerView = (RecyclerView) findViewById(R.id.recyclerBookList);
         new FirebaseDBHelper().readBooks(new FirebaseDBHelper.DataStatus() {
             @Override
@@ -41,5 +49,38 @@ public class BookList extends AppCompatActivity {
 
             }
         });
+
+        //search
+//        materialSearchBar = (MaterialSearchBar)findViewById(R.id.searchBar);
+//        loadSuggest();
+//        materialSearchBar.setCardViewElevation(10);
+//        materialSearchBar.addTextChangeListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                List<String> suggest = new ArrayList<>();
+//                for(String search:suggest){
+//                    if(search.toLowerCase().contains(materialSearchBar.getText().toLowerCase())) {
+//                        suggest.add(search);
+//                    }
+//                }
+//                materialSearchBar.setLastSuggestions(suggest);
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//
+//            }
+//        });
+
     }
+
+    private void loadSuggest() {
+
+    }
+
 }
